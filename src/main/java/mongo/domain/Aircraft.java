f1396e5ce623541194e278032aac45eaf52808cb
thsurgeonofdeath <salahek25@gmail.com>
2023-03-17 16:59:31 +0100

    database schema & connection

--- src/main/java/mongo/domain/Aircraft.java
+++ src/main/java/mongo/domain/Aircraft.java
@@ -1,4 +1,20 @@
 package mongo.domain;
 
-public class Aircraft {
+public class Aircraft{
+    private String model;
+    private int nbSeats;
+
+    public Aircraft(String model, int nbSeats) {
+        this.model = model;
+        this.nbSeats = nbSeats;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getNbSeats() {
+        return nbSeats;
+    }
 }
+
--- src/main/java/mongo/domain/FlightType.java
+++ src/main/java/mongo/domain/FlightType.java
@@ -1,4 +1,48 @@
 package mongo.domain;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class FlightType {
+    private String id;
+    private String departureCity;
+    private String destinationCity;
+    private FlightType type;
+    private boolean isDelayed;
+    private int durationMin;
+    private LocalDate departureDate;
+    private Aircraft aircraft;
+    private LocalDate crearedAt;
+
+    public FlightType(LocalDate crearedAt) {
+        this.crearedAt = LocalDate.now();
+    }
+
+    public String getDepartureCity() {
+        return departureCity;
+    }
+
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    public FlightType getType() {
+        return type;
+    }
+
+    public boolean isDelayed() {
+        return isDelayed;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public Aircraft getAircraft() {
+        return aircraft;
+    }
 }
